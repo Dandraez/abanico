8dbd188c6a7d7e14cafba5791336505c92157078
Muur <47861971+Dandraez@users.noreply.github.com>
2024-08-09 15:44:55 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+const arrayData = [
+    {
+    id: 1
+    name: "abanico"
+    description: "4 Habitaciones, Sector Villa Margarita"
+    link: ""
+    },
+    {
+    id: 2
+    name: "abanico"
+    description: "4 Habitaciones, Sector Villa Margarita"
+    link: ""
+    },
+    {
+    id: 3
+    name: "abanico"
+    description: "4 Habitaciones, Sector Villa Margarita"
+    link: ""
+    },
+    {
+    id: 4
+    name: "abanico"
+    description: "4 Habitaciones, Sector Villa Margarita"
+    link: ""
+    },
+    {
+    id: 5
+    name: "abanico"
+    description: "4 Habitaciones, Sector Villa Margarita"
+    link: ""
+    },
+    {
+    id: 6
+    name: "abanico"
+    description: "4 Habitaciones, Sector Villa Margarita"
+    link: ""
+    },
+    {
+    id: 7
+    name: "abanico"
+    description: "4 Habitaciones, Sector Villa Margarita"
+    link: ""
+    },
+];
